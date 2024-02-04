292c6b5ec29011f7bd9411227a76470961567c1e
rafaelgucky <rafaelgucky97@gmail.com>
2024-02-04 13:25:17 -0300

    update on table and grid auto-fit

--- mobilescript.js
+++ mobilescript.js
@@ -2,7 +2,5 @@ let menu = document.getElementById("hamburger-menu");
 MobileUserMenu();
 
 function MobileUserMenu(){
-    console.log(menu.style.width)
     menu.style.display = menu.style.display == "none" ? "grid" : "none";
-    menu.style.width = menu.style.width == "0%" ? "100%" : "0%";
 }
